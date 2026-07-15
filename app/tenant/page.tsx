a807da13f5b0b50d0febcc784fc0f1fcc5ba9c34
maxsash <the.revived.1@gmail.com>
2026-07-15 10:20:10 +0530

    add route for tenant

---
+++ app/tenant/page.tsx
@@ -0,0 +1,9 @@
+import TenantHome from "@/components/tenants/TenantHome";
+
+export const metadata = {
+  title: "Tenants - Shrivastava Hub",
+};
+
+export default function Page() {
+  return <TenantHome />;
+}
